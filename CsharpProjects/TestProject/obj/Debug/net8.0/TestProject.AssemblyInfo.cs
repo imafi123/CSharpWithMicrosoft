bbfc793bcc66c728eb089d29a955a0cbcb5694fa
imafi123 <imafi.i@hotmail.com>
2024-11-25 06:03:07 +0000

    Temp file updates

--- CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98f3b0a4b5693c191d61500571ee6b4f72990b06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bd5ccc11896ab9f4ef64299791fa4359206288")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
